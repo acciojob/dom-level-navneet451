11ad61be45e850717f3119a2bf7eb3b87c524bcb
Navneet_thakur <127828104+navneet451@users.noreply.github.com>
2024-10-20 12:46:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+// Function to determine the DOM level of an element
+function findDomLevel() {
+    // Get the element with id 'level'
+    let element = document.getElementById('level');
+    let level = 0;
+
+    // Traverse up the DOM tree until no parent exists (i.e., we reach the root <html> element)
+    while (element) {
+        level++;
+        element = element.parentElement;
+    }
+
+    // Display the DOM level using alert
+    alert("The level of the element is: " + level);
+}
+
+// Call the function
+findDomLevel();
